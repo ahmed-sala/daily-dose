93d5fd50a1fbf5c04da2f7456c93a856fd645707
ahmed-sala <ahmeds66210@gmail.com>
2024-09-09 00:54:57 +0300

    first commit

---
+++ lib/core/constant/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color grey = Color(0xFF8E8E8E);
+  static const Color primaryColor = Color(0xFFF2786D);
+  static const Color backgroundColor = Color(0xFFF8F9FD);
+  static const Color black = Color(0xFF000000);
+}
---
+++ lib/core/constant/imageAssets.dart
@@ -0,0 +1,8 @@
+class AppImageassets {
+  static const String rootImages = 'assets/images';
+  static const String onboardingImageOne = '$rootImages/one.PNG';
+  static const String onboardingImageTwo = '$rootImages/two.PNG';
+  static const String onboardingImageThree = '$rootImages/three.PNG';
+  static const String logo = '$rootImages/logo.png';
+}
+// assets/images/onboardingfour.PNG
